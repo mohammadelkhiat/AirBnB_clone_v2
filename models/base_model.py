22edb824940d0a21acffc28507cc98d2681b767d
Mohammad <mohammadelkhiat@gmail.com>
2024-03-20 15:04:23 +0200

    0_0

--- models/base_model.py
+++ models/base_model.py
@@ -53,6 +53,6 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        ''' delete the current instance from the storage '''
+        '''delete the current instance from the storage'''
         from models import storage
         storage.delete(self)
